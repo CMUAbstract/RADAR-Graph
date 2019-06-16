23fdf5ab3f1e913203369efa07ff2d51046e5895
vignesh <vignesh2408@gmail.com>
2019-06-16 13:13:11 -0700

    [script] Fixed compilation scripts

--- src/compile_all.py
+++ src/compile_all.py
@@ -5,7 +5,15 @@ folders = ['push-new-orig', 'push-pull', 'degree-sorting', 'hubdup', 'RADAR']
 
 for folder in folders:
     origDir = os.getcwd()
-    os.chdir(origDir + '/' + folder + '/apps')
-    subprocess.call('make clean; make -j 28', shell=True)
+    os.chdir(origDir + '/' + folder + '/Ligra/apps')
+    subprocess.call('make clean; make -j 4', shell=True)
+    os.chdir(origDir)
+
+    if 'push-pull' in folder:
+        print('~~~~ Compiled ' + folder + ' ~~~~')
+        continue
+
+    os.chdir(origDir + '/' + folder + '/GAP/')
+    subprocess.call('make clean; make -j 4', shell=True)
     os.chdir(origDir)
     print('~~~~ Compiled ' + folder + ' ~~~~')
